f409d7b9d7ad6c465a874a22f316d6b815d6a3db
Tilen Tratnjek <80243427+Tilchy@users.noreply.github.com>
2024-10-18 12:43:39 +0200

    Simplified admin creation (need refactor because of duplicated code)

--- src/users/register.py
+++ src/users/register.py
@@ -58,6 +58,52 @@ class UserRegister():
         except sqlite3.Error as e:
             logging.error(f"Error creating user: {e}")
             return {"message": f"Error creating user: {e}"}
+        
+    def create_admin(self, user: UserDto) -> None:
+        try:
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+                
+                query = "SELECT * FROM users WHERE name = ? OR email = ?"
+                cursor.execute(query, (user.name, user.email))
+                result = cursor.fetchone()
+
+                if result:
+                    logging.error(f"User with name {user.name} or email {user.email} already exists")
+                    return {"message": f"User with name {user.name} or email {user.email} already exists"}
+
+        except sqlite3.Error as e:
+            logging.error(f"Error checking if user exists: {e}")
+            return {"message": f"Error checking if user exists: {e}"}
+
+        try:
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+                
+                query = '''
+                INSERT INTO users (uuid, name, email, password)
+                VALUES (?, ?, ?, ?)
+                '''
+
+                query_add_role = "INSERT INTO user_roles (user_uuid, role_id) VALUES (?, ?)"
+
+                cursor.execute("SELECT id FROM roles WHERE name = 'admin'")
+                admin_role_id = cursor.fetchone()[0]
+
+                password_helper = PasswordHelper()
+                hashed_password = password_helper.hash_password(user.password)
+                user_uuid = str(uuid.uuid4())
+                cursor.execute(query, (user_uuid, user.name, user.email, hashed_password))
+                cursor.execute(query_add_role, (user_uuid, admin_role_id))
+
+                conn.commit()
+
+                return {"message": "Admin created successfully"}
+
+        except sqlite3.Error as e:
+            logging.error(f"Error creating admin: {e}")
+            return {"message": f"Error creating admin: {e}"}
+        
 
 if __name__ == "__main__":
     user = UserDto(name="tilch", email="GfV2Y@example.com", password="password123")
